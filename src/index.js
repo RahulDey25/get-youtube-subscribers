7e9abe4a5147b676c7107e90878370f82f23fcae
Rahul <deyr279@gmail.com>
2024-02-16 16:34:15 +0530

    port commented on index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 // Importing the app instance from the app.js file
 const app = require('./app.js');
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 // Start the server and listen for incoming requests
 app.listen(port, () => console.log(`App listening on port ${port}!`))
 
